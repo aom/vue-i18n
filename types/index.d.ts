5bec0bbbd7e0e12225867fce3d99a34c3587b6ac
Mika Marttila <mika@oranssi.org>
2017-03-28 15:22:50 +0300

    chore (typescript): Moves Vue options to index and declares mixins

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,27 @@
-// TODO: augment typings of Vue.js
-// import "./vue";
+import Vue from 'vue';
 import * as R from './vue-i18n'
 
+declare namespace VueI18n {
+  export type Translate = R.Translate
+  export type TranslateChoices = R.TranslateChoices
+  export type TranslationExists = R.TranslationExists
+}
+
 declare class VueI18n extends R.VueI18n {}
+
+declare module "vue/types/options" {
+  interface ComponentOptions<V extends Vue> {
+    i18n?: VueI18n;
+  }
+}
+
+declare module "vue/types/vue" {
+  interface Vue {
+    $i18n: VueI18n;
+    $t: VueI18n.Translate;
+    $tc: VueI18n.TranslateChoices;
+    $te: VueI18n.TranslationExists;
+  }
+}
 
 export = VueI18n;
